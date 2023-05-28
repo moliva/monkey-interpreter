02268585dc66126820635c47e84422a95a92e0df
moliva <oliva.miguelh@gmail.com>
2023-05-28 16:52:58 -0300

    chore: remove unused code

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,6 @@
 #[macro_use]
 extern crate lazy_static;
 
-pub mod token;
 pub mod lexer;
 pub mod repl;
-
-pub fn main() {
-
-}
-
-#[cfg(test)]
-mod test {
-
-    #[ignore]
-    #[test]
-    fn test() {
-        let monkey_source = r#"
-let five = 5;
-let ten = 10;
-let add = fn(x, y) {
-x + y;
-};
-let result = add(five, ten);
-"#;
-println!("source code: {}", monkey_source); }
-}
+pub mod token;
