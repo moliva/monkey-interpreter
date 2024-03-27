e14eb5582bd7e5c0c09549f22952cb31ff1067a5
moliva <oliva.miguelh@gmail.com>
2024-03-27 01:10:30 -0300

    feat: finish modify traverse

--- src/modify.rs
+++ src/modify.rs
@@ -1,6 +1,7 @@
 use crate::ast::{
-    Expression, ExpressionStatement, IndexOperator, InfixExpression, Node, PrefixExpression,
-    Program, Statement,
+    ArrayLiteral, BlockStatement, Expression, ExpressionStatement, FunctionLiteral, HashLiteral,
+    IfExpression, IndexOperator, InfixExpression, Let, Node, PrefixExpression, Program, Return,
+    Statement,
 };
 
 pub(crate) type ModifierFn = Box<dyn Fn(&mut Node)>;
@@ -26,6 +27,36 @@ pub(crate) fn modify(mut node: Node, modifier: &ModifierFn) -> Node {
                     token,
                 },
                 _ => panic!(),
+            }),
+            Statement::Block(BlockStatement { token, statements }) => {
+                Statement::Block(BlockStatement {
+                    token,
+                    statements: statements
+                        .into_iter()
+                        .map(|s| match modify(Node::Statement(s), modifier) {
+                            Node::Statement(s) => s,
+                            _ => panic!(),
+                        })
+                        .collect(),
+                })
+            }
+            Statement::Return(Return {
+                token,
+                return_value,
+            }) => Statement::Return(Return {
+                token,
+                return_value: match modify(Node::Expression(return_value), modifier) {
+                    Node::Expression(e) => e,
+                    _ => panic!(),
+                },
+            }),
+            Statement::Let(Let { token, name, value }) => Statement::Let(Let {
+                token,
+                name,
+                value: match modify(Node::Expression(value), modifier) {
+                    Node::Expression(e) => e,
+                    _ => panic!(),
+                },
             }),
             s => s,
         }),
@@ -72,6 +103,81 @@ pub(crate) fn modify(mut node: Node, modifier: &ModifierFn) -> Node {
                     }),
                 })
             }
+            Expression::If(IfExpression {
+                token,
+                condition,
+                consequence,
+                alternative,
+            }) => Expression::If(IfExpression {
+                token,
+                condition: Box::new(match modify(Node::Expression(*condition), modifier) {
+                    Node::Expression(e) => e,
+                    _ => panic!(),
+                }),
+                consequence: match modify(Node::Statement(Statement::Block(consequence)), modifier)
+                {
+                    Node::Statement(Statement::Block(s)) => s,
+                    _ => panic!(),
+                },
+                alternative: alternative.map(|alternative| {
+                    match modify(Node::Statement(Statement::Block(alternative)), modifier) {
+                        Node::Statement(Statement::Block(s)) => s,
+                        _ => panic!(),
+                    }
+                }),
+            }),
+            Expression::FunctionLiteral(FunctionLiteral {
+                token,
+                parameters,
+                body,
+            }) => Expression::FunctionLiteral(FunctionLiteral {
+                token,
+                parameters: parameters
+                    .into_iter()
+                    .map(
+                        |p| match modify(Node::Expression(Expression::Identifier(p)), modifier) {
+                            Node::Expression(Expression::Identifier(p)) => p,
+                            _ => panic!(),
+                        },
+                    )
+                    .collect(),
+                body: match modify(Node::Statement(Statement::Block(body)), modifier) {
+                    Node::Statement(Statement::Block(s)) => s,
+                    _ => panic!(),
+                },
+            }),
+            Expression::ArrayLiteral(ArrayLiteral { token, elements }) => {
+                Expression::ArrayLiteral(ArrayLiteral {
+                    token,
+                    elements: elements
+                        .into_iter()
+                        .map(|e| match modify(Node::Expression(e), modifier) {
+                            Node::Expression(e) => e,
+                            _ => panic!(),
+                        })
+                        .collect(),
+                })
+            }
+            Expression::HashLiteral(HashLiteral { token, pairs }) => {
+                Expression::HashLiteral(HashLiteral {
+                    token,
+                    pairs: pairs
+                        .into_iter()
+                        .map(|(k, v)| {
+                            (
+                                match modify(Node::Expression(k), modifier) {
+                                    Node::Expression(e) => e,
+                                    _ => panic!(),
+                                },
+                                match modify(Node::Expression(v), modifier) {
+                                    Node::Expression(e) => e,
+                                    _ => panic!(),
+                                },
+                            )
+                        })
+                        .collect(),
+                })
+            }
             s => s,
         }),
     };
@@ -86,8 +192,9 @@ pub(crate) fn modify(mut node: Node, modifier: &ModifierFn) -> Node {
 mod test {
     use crate::{
         ast::{
-            Expression, ExpressionStatement, IndexOperator, InfixExpression, IntegerLiteral, Node,
-            PrefixExpression, Program, Statement,
+            ArrayLiteral, BlockStatement, Expression, ExpressionStatement, FunctionLiteral,
+            HashLiteral, Identifier, IfExpression, IndexOperator, InfixExpression, IntegerLiteral,
+            Let, Node, PrefixExpression, Program, Return, Statement,
         },
         token::Token,
     };
@@ -170,6 +277,116 @@ mod test {
                     token: Token::Lt,
                     left: Box::new(two()),
                     index: Box::new(two()),
+                })),
+            ),
+            (
+                Node::Expression(Expression::If(IfExpression {
+                    token: Token::Lt,
+                    condition: Box::new(one()),
+                    consequence: BlockStatement {
+                        token: Token::Lt,
+                        statements: vec![Statement::Expression(ExpressionStatement {
+                            token: Token::Lt,
+                            expression: one(),
+                        })],
+                    },
+                    alternative: Some(BlockStatement {
+                        token: Token::Lt,
+                        statements: vec![Statement::Expression(ExpressionStatement {
+                            token: Token::Lt,
+                            expression: one(),
+                        })],
+                    }),
+                })),
+                Node::Expression(Expression::If(IfExpression {
+                    token: Token::Lt,
+                    condition: Box::new(two()),
+                    consequence: BlockStatement {
+                        token: Token::Lt,
+                        statements: vec![Statement::Expression(ExpressionStatement {
+                            token: Token::Lt,
+                            expression: two(),
+                        })],
+                    },
+                    alternative: Some(BlockStatement {
+                        token: Token::Lt,
+                        statements: vec![Statement::Expression(ExpressionStatement {
+                            token: Token::Lt,
+                            expression: two(),
+                        })],
+                    }),
+                })),
+            ),
+            (
+                Node::Statement(Statement::Return(Return {
+                    token: Token::Lt,
+                    return_value: one(),
+                })),
+                Node::Statement(Statement::Return(Return {
+                    token: Token::Lt,
+                    return_value: two(),
+                })),
+            ),
+            (
+                Node::Statement(Statement::Let(Let {
+                    token: Token::Lt,
+                    name: Identifier {
+                        token: Token::Lt,
+                        value: "some".to_owned(),
+                    },
+                    value: one(),
+                })),
+                Node::Statement(Statement::Let(Let {
+                    token: Token::Lt,
+                    name: Identifier {
+                        token: Token::Lt,
+                        value: "some".to_owned(),
+                    },
+                    value: two(),
+                })),
+            ),
+            (
+                Node::Expression(Expression::FunctionLiteral(FunctionLiteral {
+                    token: Token::Lt,
+                    parameters: Vec::default(),
+                    body: BlockStatement {
+                        token: Token::Lt,
+                        statements: vec![Statement::Expression(ExpressionStatement {
+                            token: Token::Lt,
+                            expression: one(),
+                        })],
+                    },
+                })),
+                Node::Expression(Expression::FunctionLiteral(FunctionLiteral {
+                    token: Token::Lt,
+                    parameters: Vec::default(),
+                    body: BlockStatement {
+                        token: Token::Lt,
+                        statements: vec![Statement::Expression(ExpressionStatement {
+                            token: Token::Lt,
+                            expression: two(),
+                        })],
+                    },
+                })),
+            ),
+            (
+                Node::Expression(Expression::ArrayLiteral(ArrayLiteral {
+                    token: Token::Lt,
+                    elements: vec![one(), one()],
+                })),
+                Node::Expression(Expression::ArrayLiteral(ArrayLiteral {
+                    token: Token::Lt,
+                    elements: vec![two(), two()],
+                })),
+            ),
+            (
+                Node::Expression(Expression::HashLiteral(HashLiteral {
+                    token: Token::Lt,
+                    pairs: vec![(one(), one()), (one(), one())],
+                })),
+                Node::Expression(Expression::HashLiteral(HashLiteral {
+                    token: Token::Lt,
+                    pairs: vec![(two(), two()), (two(), two())],
                 })),
             ),
         ];
